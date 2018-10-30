1ed79add3f3348eca1f6edb8308a3a1939137b69
Alexander <amire51117@gmail.com>
2018-10-30 16:25:05 -0500

    complete every question, pre-test

--- src/main/java/com/revature/simple/JavaSimpleSolution.java
+++ src/main/java/com/revature/simple/JavaSimpleSolution.java
@@ -1,5 +1,8 @@
 package com.revature.simple;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class JavaSimpleSolution implements JavaSimple {
 
 	@Override
@@ -13,9 +16,10 @@ public class JavaSimpleSolution implements JavaSimple {
 	@Override
 	public byte castToByte(short n) {
 		// TODO Auto-generated method stub
+		byte byteNum = (byte) n;
 		
 		
-		return 0;
+		return byteNum;
 	}
 
 	@Override
@@ -56,8 +60,9 @@ public class JavaSimpleSolution implements JavaSimple {
 		for (int i = 0; i < array.length; i++) {
 			total += array[i];
 		}
+		double averageNum = array.length;
 		
-		return total / array.length;
+		return total / averageNum;
 	}
 
 	@Override
@@ -65,7 +70,7 @@ public class JavaSimpleSolution implements JavaSimple {
 		// TODO Auto-generated method stub
 		int maxNum = array[0];
 		
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 1; i < array.length; i++) {
 			if (array[i] > maxNum) {
 				maxNum = array[i];
 			}
@@ -97,9 +102,34 @@ public class JavaSimpleSolution implements JavaSimple {
 	@Override
 	public int[] sort(int[] array) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
+		
+		for (int index = array.length - 1; index > 1; index--) {
+			
+			int maxNum = findMaxInt(array, index); 
+			int tempNum = array[maxNum];
+	
+			array[maxNum] = array[index];
+			array[index] = tempNum;
+		}	
 		return null;
 	}
 
+	public int findMaxInt(int[] arr, int maxInt) {
+		if (maxInt < 1) {
+			return 0;
+		}
+		int max = 0;
+		
+		
+		for (int i = 0; i <= maxInt; i++) {
+			if (arr[i] > arr[max]) {
+				max = i;
+			}
+		}
+
+		return max;
+	}
+	
 	@Override
 	public int factorial(int n) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
@@ -143,13 +173,50 @@ public class JavaSimpleSolution implements JavaSimple {
 	@Override
 	public String isPrime(int n) {
 		// TODO Auto-generated method stub
-		return null;
+		String yes = "Is Prime";
+		String no = "Not Prime";
+		String answer = yes;
+		
+		
+		answer =  isEven(n) ? no : yes;
+		
+		answer = n % 3 == 0 || answer == no ? no : yes;
+		
+		for (int i = 5; i * i <= n; i += 6) {
+			answer = (n % i == 0 || (i + 2) == 0 ) ? no : yes;
+		}
+
+		return answer;
 	}
 
 	@Override
 	public boolean balancedBrackets(String brackets) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
-		return false;
+		
+		int strSize = brackets.length();
+		
+		Deque<Character> stack = new ArrayDeque<Character>();
+		
+		for (int i = 0; i < strSize; i++) {
+			char cha = brackets.charAt(i);
+			
+			if (cha == ")".charAt(0)) {
+				if (stack.poll()!="(".charAt(0)) {
+					return false;
+				}
+			} else if (cha == "]".charAt(0)) {
+				if (stack.poll()!="[".charAt(0)) {
+					return false;
+				}
+			} else if (cha == "}".charAt(0)) {
+				if (stack.poll()!="{".charAt(0)) {
+					return false;
+				}
+			} else {				
+				stack.push(cha);
+			}
+		}
+		return stack.size() == 0 ? true : false;
 	}
 
 }
