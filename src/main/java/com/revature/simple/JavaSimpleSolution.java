1b72030e4f7862dae11cc116c2f79306d11743c8
Alexander <amire51117@gmail.com>
2018-10-31 16:13:34 -0500

    fix some issues

--- src/main/java/com/revature/simple/JavaSimpleSolution.java
+++ src/main/java/com/revature/simple/JavaSimpleSolution.java
@@ -2,9 +2,21 @@ package com.revature.simple;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+//import org.apache.log4j.Logger;
+import com.revature.simple.JavaSimpleSolution;
 
 public class JavaSimpleSolution implements JavaSimple {
 
+//	private static final Logger LOGGER = Logger.getLogger(JavaSimpleSolution.class);	
+	
+	public static void main(String[] args) {
+		JavaSimpleSolution jss = new JavaSimpleSolution();
+		
+		//		LOGGER.info(jss.fibonacci(4));
+		//	System.out.println(jss.factorial(3));
+		//	System.out.println(jss.isPrime(51));
+//			System.out.println(jss.balancedBrackets("(}]("));
+	}
 	@Override
 	public int castToInt(double n) {
 		// TODO Auto-generated method stub
@@ -89,14 +101,14 @@ public class JavaSimpleSolution implements JavaSimple {
 		}
 		int newNum = 1;
 		int oldNum = 1;
-		int latest = 2;
+		int latest;
 		
 		for (int counter = 3; counter < n; counter++) {
 			latest = newNum + oldNum;
 			oldNum = newNum;
 			newNum = latest;
 		}
-		return latest;
+		return newNum + oldNum;
 	}
 
 	@Override
@@ -171,18 +183,21 @@ public class JavaSimpleSolution implements JavaSimple {
 	}
 
 	@Override
-	public String isPrime(int n) {
+	public String isPrime(int n) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
+		
+		if (n < 0) {
+			throw new IllegalArgumentException("int must be larger than 0!");
+		}
 		String yes = "Is Prime";
 		String no = "Not Prime";
 		String answer = yes;
 		
+		answer = n == 1 || n == 0 ? no : answer;
+		answer =  n % 2 == 0 && n != 2 ? no : answer;
+		answer = n % 3 == 0 && n != 3 ? no : answer;
 		
-		answer =  isEven(n) ? no : answer;
-		
-		answer = n % 3 == 0 || answer == no ? no : answer;
-		
-		for (int i = 5; i * i <= n; i += 6) {
+		for (int i = 5; i * i <= n; i += 2) {
 			answer = (n % i == 0 || (i + 2) == 0 ) ? no : answer;
 		}
 
@@ -192,29 +207,34 @@ public class JavaSimpleSolution implements JavaSimple {
 	@Override
 	public boolean balancedBrackets(String brackets) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
-		
+
 		int strSize = brackets.length();
 		
 		Deque<Character> stack = new ArrayDeque<Character>();
+		String opening = "({[";
+		String closing = "})]";
 		
 		for (int i = 0; i < strSize; i++) {
 			char cha = brackets.charAt(i);
+			String str = "" + cha;
 			
-			if (cha == ")".charAt(0)) {
-				if (stack.poll()!="(".charAt(0)) {
-					return false;
-				}
-			} else if (cha == "]".charAt(0)) {
-				if (stack.poll()!="[".charAt(0)) {
-					return false;
-				}
-			} else if (cha == "}".charAt(0)) {
-				if (stack.poll()!="{".charAt(0)) {
-					return false;
-				}
-			} else {				
+			if (opening.contains(str)) {
 				stack.push(cha);
-			}
+			} else if (stack.size() == 0 && closing.contains(str)) {
+				return false;
+			} else if (closing.contains(str)) {
+				// stack size exists and it contains closing brackets
+				Character firstChar = stack.pop();
+				
+				if (cha == ')' && firstChar != '(') {
+					return false;
+				} else if (cha == ']' && firstChar != '[') {
+					return false;
+				} else if (cha == '}' && firstChar != '{') {
+					return false;
+				}
+			}		
+
 		}
 		return stack.size() == 0 ? true : false;
 	}
